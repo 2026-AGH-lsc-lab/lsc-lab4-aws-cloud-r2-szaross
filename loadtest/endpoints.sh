cd0391b9fdd916f746f440e8ae9b65d98d3119e4
EC2 Default User <ec2-user@ip-172-31-10-221.ec2.internal>
2026-03-23 18:32:57 +0000

    remote ec2 results

--- loadtest/endpoints.sh
+++ loadtest/endpoints.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Endpoint URLs for load testing
 # Fill in after deploying with the URLs printed by each deploy script
-export LAMBDA_ZIP_URL=""        # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export LAMBDA_CONTAINER_URL=""  # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export FARGATE_URL=""           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
-export EC2_URL=""               # e.g. http://<public-ip>:8080
+export LAMBDA_ZIP_URL="https://52h5moavfwi45qw2zoepk7oq540hwutf.lambda-url.us-east-1.on.aws"        # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export LAMBDA_CONTAINER_URL="https://to4mb6orcbpmluwh6v2ci6dyji0fgvvu.lambda-url.us-east-1.on.aws"  # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export FARGATE_URL="http://lsc-knn-alb-1742105879.us-east-1.elb.amazonaws.com"           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
+export EC2_URL="http://44.221.79.18:8080"               # e.g. http://<public-ip>:8080
